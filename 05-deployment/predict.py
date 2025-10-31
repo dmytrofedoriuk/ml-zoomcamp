7ca51e00f3509f9fee75bacf87d9a96964c5f94c
dmytrofedoriuk <98764239+dmytrofedoriuk@users.noreply.github.com>
2025-10-31 03:47:02 +0000

    add homework for 05-deployment module

---
+++ 05-deployment/predict.py
@@ -0,0 +1,27 @@
+import pickle
+import uvicorn
+from fastapi import FastAPI
+from typing import Dict, Any
+
+app = FastAPI(title="lead_conversion_prediction")
+
+with open('pipeline_v2.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+
+def predict_single(customer):
+    result = pipeline.predict_proba(customer)[0, 1]
+    return float(result)
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    prob = predict_single(customer)
+
+    return {
+        "lead_conversion_probability": prob,
+        "converted": bool(prob >= 0.5)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
